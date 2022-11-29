51ca8cc925148fbb47ae90e209c7b7bec53f9fc1
Siddharth Bhatia <32610387+Sid-Bhatia-0@users.noreply.github.com>
2022-11-30 02:05:07 +0530

    add custom keybinding for clearing REPL screen

--- startup.jl
+++ startup.jl
@@ -1,3 +1,7 @@
+import REPL
+import REPL.LineEdit
+import REPL.Terminals
+
 try
     import OhMyREPL
 catch e
@@ -15,3 +19,13 @@ try
 catch e
     @warn "Error initializing Debugger" exception=(e, catch_backtrace())
 end
+
+const mykeys = Dict{Any,Any}(
+    "^O" => (s,o...)->(Terminals.clear(LineEdit.terminal(s)); LineEdit.refresh_line(s)),
+)
+
+function customize_keys(repl)
+    repl.interface = REPL.setup_interface(repl; extra_repl_keymap = mykeys)
+end
+
+atreplinit(customize_keys)
